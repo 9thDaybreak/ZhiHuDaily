ea38cb9e1de00ee6d7f2ac3cae89796da307855d
QiGuanYiChen <450552846@qq.com>
2016-08-15 18:06:09 +0800

    数据工具

---
+++ ZhiHuDaily/GNDataUtils.h
@@ -0,0 +1,17 @@
+//
+//  GNDataUtils.h
+//  ZhiHuDaily
+//
+//  Created by 肖杰华 on 16/8/15.
+//  Copyright © 2016年 ZhuSunGongZuoShi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GNDataUtils : NSObject
+
++ (NSString *)todayDateString;
+
++ (NSString *)dateStringBeforeDays:(NSInteger)days;
+
+@end
---
+++ ZhiHuDaily/GNDataUtils.m
@@ -0,0 +1,31 @@
+//
+//  GNDataUtils.m
+//  ZhiHuDaily
+//
+//  Created by 肖杰华 on 16/8/15.
+//  Copyright © 2016年 ZhuSunGongZuoShi. All rights reserved.
+//
+
+#import "GNDataUtils.h"
+
+@implementation GNDataUtils
+
++(NSString *)todayDateString {
+    NSDate *today = [NSDate date];
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"yyyyMMdd"];
+    
+    //返回现在时间的字符串
+    return [formatter stringFromDate:today];
+}
+
++(NSString *)dateStringBeforeDays:(NSInteger)days {
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"yyyyMMdd"];
+    //获取到 days 天前的date
+    NSDate *before = [NSDate dateWithTimeIntervalSinceNow:-days*60*60*24];
+    ////返回 days 天前时间的字符串
+    return [formatter stringFromDate:before];
+}
+
+@end
